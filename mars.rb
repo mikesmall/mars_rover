57676296c191784e60561a66ac11d1543bba5f33
mikesmall <michael.l.a.small@gmail.com>
2017-08-15 21:23:57 -0400

    Empty methods for accepting instruction, turning the rover, and moving the rover

---
+++ mars.rb
@@ -0,0 +1,21 @@
+class Rover
+
+  def initialize
+    @x_coordinate = 0
+    @y_coordinate = 0
+    @direction    = 'n'
+  end
+
+  def read_instruction(instruction)
+
+  end
+
+  def move # affects position of rover
+
+  end
+
+  def turn # affects direction of rover
+
+  end
+
+end
