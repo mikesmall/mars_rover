8534a31c0adf5cca1ffae67d461fc508443cd3e9
mikesmall <michael.l.a.small@gmail.com>
2017-08-17 17:29:54 -0400

    Refactored 'direction_to_string' into its own method

--- mars.rb
+++ mars.rb
@@ -11,6 +11,19 @@ class Rover
     @text_filename      = text_filename
   end
 
+  def direction_to_string
+    @direction_to_string = ""
+      if @direction == "N"
+        @direction_to_string = "North"
+      elsif @direction == "E"
+        @direction_to_string = "East"
+      elsif @direction == "W"
+        @direction_to_string = "West"
+      elsif @direction == "S"
+        @direction_to_string = "South"
+      end
+  end
+
   def get_file
     # For now, accept string entered by user:
     puts "Type or paste your entire input: "
@@ -25,17 +38,19 @@ class Rover
 
   def read_instruction
     # Split file contents into array
-    @instructions = @text_filename.split
+    @instructions = @text_filename.chars
 
     # First two characters establish upper right corner
     # (Lower left is assumed to be 0, 0)
     @upper_right_corner = @instructions[0,1]
+    puts "Upper-right corner of plateau: X(#{@upper_right_corner[0]}), Y(#{@upper_right_corner[1]})"
 
     # Initial Rover placement:
     @x_coordinate = @instructions[2]
     @y_coordinate = @instructions[3]
     @direction = @instructions[4]
-
+    direction_to_string
+    puts "A Mars Rover landed at X-coordinate #{@x_coordinate} and Y-coordinate #{@y_coordinate}, facing #{@direction_to_string}."
     # Rover movement:
     @instructions.each do |character|
       if character == "L"
@@ -59,7 +74,7 @@ class Rover
     elsif @direction == "E"
       @direction = "N"
     end
-    puts "The Mars Rover turned left."
+    puts "The Rover turned left."
   end
 
   def turn_right
@@ -72,7 +87,7 @@ class Rover
     elsif @direction == "W"
       @direction = "N"
     end
-    puts "The Mars Rover turned right."
+    puts "The Rover turned right."
   end
 
   def move
@@ -85,21 +100,12 @@ class Rover
     elsif direction == "W"
       @x_coordinate -= 1
     end
-    puts "The Mars Rover moved forward."
+    puts "The Rover moved forward."
   end
 
   def give_result
-    @direction_to_string = ""
-    if @direction == "N"
-      @direction_to_string = "North"
-    elsif @direction == "E"
-      @direction_to_string = "East"
-    elsif @direction == "W"
-      @direction_to_string = "West"
-    elsif @direction == "S"
-      @direction_to_string = "South"
-    end
-    puts "The Mars Rover has arrived at X-coordinate #{@x_coordinate} and Y-coordinate #{@y_coordinate}, facing #{@direction_to_string}."
+    direction_to_string
+    puts "The Rover has arrived at X-coordinate #{@x_coordinate} and Y-coordinate #{@y_coordinate}, facing #{@direction_to_string}."
   end
 
 end
