35524058fc75147de5b5413f7a3e4566966f965a
mikesmall <michael.l.a.small@gmail.com>
2017-08-17 17:21:10 -0400

    Program runs but Rover doesn't 'move' yet

--- mars.rb
+++ mars.rb
@@ -1,51 +1,48 @@
 class Rover
 
-  attr_accessor :x_coordinate, :y_coordinate, :direction, :text_filename, :upper_right_corner
+  attr_accessor :x_coordinate, :y_coordinate, :direction, :text_filename, :upper_right_corner, :instructions
 
   def initialize
     @lower_left_corner  = [0, 0]
     @upper_right_corner = []
     @x_coordinate       = 0
     @y_coordinate       = 0
-    @direction          = ''
-    @text_filename      = ''
+    @direction          = ""
+    @text_filename      = text_filename
   end
 
   def get_file
     # For now, accept string entered by user:
-    puts 'Type or paste your entire input: '
-    @text_filename = gets.chomp.to_s
+    puts "Type or paste your entire input: "
+    @text_filename = gets.chomp
     # Future file-acceptance code:
-    # puts 'Type the name of your input file (e.g. input.txt): '
+    # puts "Type the name of your input file (e.g. input.txt): "
     # @text_filename = gets.chomp.to_s
     # File.open(@text_filename).each do |line|
-    #  @input = line.split('')
+    #  @input = line.split("")
     read_instruction
   end
 
   def read_instruction
     # Split file contents into array
-    @raw_input = @text_filename.split
+    @instructions = @text_filename.split
 
-    # Convert first two characters to integers -- they will establish upper right corner (5, 5)
-    # (the lower left is assumed to be 0, 0)
-    # Store in a variable for the upper right corner (This information doesn't seem useful to keep)
-    @upper_right_corner = @text_filename[0,1]
+    # First two characters establish upper right corner
+    # (Lower left is assumed to be 0, 0)
+    @upper_right_corner = @instructions[0,1]
 
-    # Remove those two integers:
-    @rover_movement = @raw_input.shift(2)
+    # Initial Rover placement:
+    @x_coordinate = @instructions[2]
+    @y_coordinate = @instructions[3]
+    @direction = @instructions[4]
 
-    # Get initial placement:
-    @x_coordinate = @rover_movement[0].to_i
-    @y_coordinate = @rover_movement[1].to_i
-
-    # Iterate over the array:
-    @rover_movement.each do |character|
-      if character == 'L'
+    # Rover movement:
+    @instructions.each do |character|
+      if character == "L"
         turn_left
-      elsif character == 'R'
+      elsif character == "R"
         turn_right
-      elsif character == 'M'
+      elsif character == "M"
         move
       end
     end
@@ -53,56 +50,56 @@ class Rover
   end
 
   def turn_left
-    if @direction == 'N'
-      @direction = 'W'
-    elsif @direction == 'W'
-      @direction = 'S'
-    elsif @direction == 'S'
-      @direction = 'E'
-    elsif @direction == 'E'
-      @direction = 'N'
+    if @direction == "N"
+      @direction = "W"
+    elsif @direction == "W"
+      @direction = "S"
+    elsif @direction == "S"
+      @direction = "E"
+    elsif @direction == "E"
+      @direction = "N"
     end
-    puts 'The Mars Rover turned left.'
+    puts "The Mars Rover turned left."
   end
 
   def turn_right
-    if @direction == 'N'
-      @direction = 'E'
-    elsif @direction == 'E'
-      @direction = 'S'
-    elsif @direction == 'S'
-      @direction = 'W'
-    elsif @direction == 'W'
-      @direction = 'N'
+    if @direction == "N"
+      @direction = "E"
+    elsif @direction == "E"
+      @direction = "S"
+    elsif @direction == "S"
+      @direction = "W"
+    elsif @direction == "W"
+      @direction = "N"
     end
-    puts 'The Mars Rover turned right.'
+    puts "The Mars Rover turned right."
   end
 
   def move
-    if direction == 'N'
+    if direction == "N"
       @y_coordinate += 1
-    elsif direction == 'E'
+    elsif direction == "E"
       @x_coordinate += 1
-    elsif direction == 'S'
+    elsif direction == "S"
       @y_coordinate -= 1
-    elsif direction == 'W'
+    elsif direction == "W"
       @x_coordinate -= 1
     end
-    puts 'The Mars Rover moved forward.'
+    puts "The Mars Rover moved forward."
   end
 
   def give_result
-    @direction_to_string = ''
-    if @direction == 'N'
-      @direction_to_string = 'North'
-    elsif @direction == 'E'
-      @direction_to_string = 'East'
-    elsif @direction == 'W'
-      @direction_to_string = 'West'
-    elsif @direction == 'S'
-      @direction_to_string = 'South'
+    @direction_to_string = ""
+    if @direction == "N"
+      @direction_to_string = "North"
+    elsif @direction == "E"
+      @direction_to_string = "East"
+    elsif @direction == "W"
+      @direction_to_string = "West"
+    elsif @direction == "S"
+      @direction_to_string = "South"
     end
-    puts 'The Mars Rover has arrived at X-coordinate #{@x_coordinate} and Y-coordinate #{@y_coordinate}, facing #{@direction}.'
+    puts "The Mars Rover has arrived at X-coordinate #{@x_coordinate} and Y-coordinate #{@y_coordinate}, facing #{@direction_to_string}."
   end
 
 end
